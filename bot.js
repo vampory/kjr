69b49aced3e315681af71b4e940eeb9a0ce5ae51
vampory <44905458+vampory@users.noreply.github.com>
2018-12-07 19:06:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
-const { Client, Util } = require('discord.js');
-const YouTube = require('simple-youtube-api');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const ytdl = require('ytdl-core');
+const request = require('request');
+const fs = require('fs');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
 
 const client = new Client({ disableEveryone: true });
 const GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
